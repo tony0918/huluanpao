7d5e64d11f3337a5fd54ab11cda32ce12cc8ac06
Tony Mao <huiliumao@gmail.com>
2017-05-04 00:22:54 +0800

    Adding 2017 we are young initial page

--- huluanpao/themes/huluanpao/js/footer.js
+++ huluanpao/themes/huluanpao/js/footer.js
@@ -1,28 +1,33 @@
 (function ($) {
-    $(function () {
-        function handle(delta) {
-            if (delta < 0) {
-                $(".globalFooter").addClass("up");
-            } else {
-                $(".globalFooter").removeClass("up");
-            }
-        }
+  $(function () {
+    function handle(delta) {
+      if (delta < 0) {
+        $(".globalFooter").addClass("up");
+      } else {
+        $(".globalFooter").removeClass("up");
+      }
+    }
 
-        function wheel(event) {
-            var delta = 0;
-            if (!event) event = window.event;
-            if (event.wheelDelta) {
-                delta = event.wheelDelta / 120;
-                if (window.opera) delta = -delta;
-            } else if (event.detail) {
-                delta = -event.detail / 3;
-            }
-            if (delta)
-                handle(delta);
-        }
+    function wheel(event) {
+      var delta = 0;
+      if (!event) event = window.event;
+      if (event.wheelDelta) {
+        delta = event.wheelDelta / 120;
+        if (window.opera) delta = -delta;
+      } else if (event.detail) {
+        delta = -event.detail / 3;
+      }
+      if (delta)
+        handle(delta);
+    }
 
-        if (window.addEventListener)
-            window.addEventListener('DOMMouseScroll', wheel, false);
-        window.onmousewheel = document.onmousewheel = wheel;
-    });
+    if (!$('.wry-container').length) {
+      if (window.addEventListener)
+        window.addEventListener('DOMMouseScroll', wheel, false);
+      window.onmousewheel = document.onmousewheel = wheel;
+    } else {
+      $('body').addClass('wry');
+    }
+
+  });
 })(jQuery);
--- huluanpao/themes/huluanpao/js/main.js
+++ huluanpao/themes/huluanpao/js/main.js
@@ -1,23 +1,25 @@
 (function ($) {
-    $(function () {
-        $(".mainNav li").each(function (ind, el) {
-            $(el).mouseenter(function () {
-                $(el).find(".dropNav").fadeIn();
-            }).mouseleave(function () {
-                $(el).find(".dropNav").fadeOut();
-            });
-            //$(el).on("tap",function(){
-            //    $(this).find(".dropNav").toggle();
-            //})
-        });
-
-        var setHeight = function () {
-            var windowH = $(window).height();
-            $(".kvImg").css("height", windowH);
-        };
-        setHeight();
-        $(window).resize(function () {
-            setHeight();
-        });
+  $(function () {
+    $(".mainNav li").each(function (ind, el) {
+      $(el).mouseenter(function () {
+        $(el).find(".dropNav").fadeIn();
+      }).mouseleave(function () {
+        $(el).find(".dropNav").fadeOut();
+      });
+      //$(el).on("tap",function(){
+      //    $(this).find(".dropNav").toggle();
+      //})
     });
+
+    var setHeight = function () {
+      var windowH = $(window).height();
+      $(".kvImg").css("height", windowH);
+      $(".wry-container").css("height", windowH);
+      $(".wry-article-content-wrapper").css("height", windowH - 240);
+    };
+    setHeight();
+    $(window).resize(function () {
+      setHeight();
+    });
+  });
 })(jQuery);
